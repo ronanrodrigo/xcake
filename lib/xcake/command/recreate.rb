106509911f7d809bcd60a0792896269a10983379
James Campbell <james@supmenow.com>
2016-09-12 12:01:37 +0100

    Prpearing for new recreate functioPrpearing for new recreate functionn

--- lib/xcake/command/recreate.rb
+++ lib/xcake/command/recreate.rb
@@ -1,3 +1,5 @@
+require 'xcodeproj'
+
 module Xcake
   class Command
     class Recreate < Command
@@ -5,7 +7,8 @@ module Xcake
       self.description = 'Generates a new Cakefile by recreating an existing xcode project'
 
       def run
-        puts "Coming Soon"
+        project_path = '/your_path/your_project.xcodeproj'
+        project = Xcodeproj::Project.open(project_path)
       end
     end
   end
